c42a692e4eba29e3e254409727569786e6ce4f4f
vadimstr102 <vadimstr102@gmail.com>
2019-10-09 21:34:11 +0700

    Ипотека.[#185546]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        while(amount > 0) {
+            amount = amount + (int)(amount * percent / 100) - salary;
+            year++;
+        }
+        return year;
+    }
+}
