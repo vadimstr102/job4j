1e5563fff2aa70a7b2ae681e85322540e09a517b
vadimstr102 <vadimstr102@gmail.com>
2019-10-09 23:35:57 +0700

    Простые числа.[#185545] (исправление 2)

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,10 +4,8 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
-        boolean prime;
         for (int i = 2; i <= finish; i++) {
-            prime = checkPrimeNumber.check(i);
-            if (prime) {
+            if (checkPrimeNumber.check(i)) {
                 count++;
             }
         }
