ffc2c63a8c1d2478dfa633f7a8dd0f6a5b22c8a0
vadimstr102 <vadimstr102@gmail.com>
2019-10-09 23:23:13 +0700

    Простые числа.[#185545] (исправление)

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,9 +3,10 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public boolean check(int num) {
         boolean prime = true;
-        for(int i = 2; i < num; i++) {
-            if(num % i == 0) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
                 prime = false;
+                break;
             }
         }
         return prime;
--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 0;
-        while(amount > 0) {
-            amount = amount + (int)(amount * percent / 100) - salary;
+        while (amount > 0) {
+            amount = amount + (int) (amount * percent / 100) - salary;
             year++;
         }
         return year;
