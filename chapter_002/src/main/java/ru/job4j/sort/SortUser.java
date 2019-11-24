aac66e40ca2ef828451d1f23b640561fb4201797
vadimstr102 <vadimstr102@gmail.com>
2019-11-24 16:06:28 +0700

    2. Сортировка User с использованием Comparator[#185615]

--- chapter_002/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j.sort;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -9,5 +10,26 @@ public class SortUser {
         Set<User> result = new TreeSet<>();
         result.addAll(list);
         return result;
+    }
+
+    public List<User> sortNameLength(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getName().length(), o2.getName().length());
+            }
+        });
+        return list;
+    }
+
+    public List<User> sortByAllFields(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int result = o1.getName().compareTo(o2.getName());
+                return result == 0 ? Integer.compare(o1.getAge(), o2.getAge()) : result;
+            }
+        });
+        return list;
     }
 }
--- chapter_002/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_002/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 public class SortUserTest {
     @Test
-    public void whenSorted() {
+    public void whenSortByAge() {
         List<User> users = new ArrayList<>();
         users.add(new User("Vadim", 30));
         users.add(new User("Petr", 25));
@@ -17,5 +17,27 @@ public class SortUserTest {
         users.add(new User("Alex", 18));
         Set<User> result = new SortUser().sort(users);
         Assert.assertEquals(result.toString(), "[Alex: 18, Petr: 25, Vadim: 30, Ivan: 35]");
+    }
+
+    @Test
+    public void whenSortByNameLength() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Vadim", 30));
+        users.add(new User("Petr", 25));
+        users.add(new User("Alexandr", 18));
+        users.add(new User("Cat", 35));
+        users = new SortUser().sortNameLength(users);
+        Assert.assertEquals(users.toString(), "[Cat: 35, Petr: 25, Vadim: 30, Alexandr: 18]");
+    }
+
+    @Test
+    public void whenSortByAllFields() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Vadim", 25));
+        users.add(new User("Petr", 30));
+        users.add(new User("Vadim", 20));
+        users.add(new User("Petr", 25));
+        users = new SortUser().sortByAllFields(users);
+        Assert.assertEquals(users.toString(), "[Petr: 25, Petr: 30, Vadim: 20, Vadim: 25]");
     }
 }
