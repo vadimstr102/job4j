f91b4114329ef662ff7ea045302d0df14397491a
vadimstr102 <vadimstr102@gmail.com>
2019-11-28 21:22:55 +0700

    Отсортировать департаменты [#34352]

---
+++ chapter_002/src/test/java/ru/job4j/sort/DepartureTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.sort;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartureTest {
+    List<String> list = new ArrayList<>();
+
+    @Before
+    public void start() {
+        list.add("K1\\SK1");
+        list.add("K1\\SK2");
+        list.add("K1\\SK1\\SSK1");
+        list.add("K1\\SK1\\SSK2");
+        list.add("K2");
+        list.add("K2\\SK1\\SSK1");
+        list.add("K2\\SK1\\SSK2");
+    }
+
+    @Test
+    public void whenFillGapsAndAbs() {
+        list = Departure.fillGaps(list);
+        Departure.abs(list);
+        Assert.assertEquals(list.toString(), "[K1, K1\\SK1, K1\\SK1\\SSK1, K1\\SK1\\SSK2, K1\\SK2, K2, K2\\SK1, K2\\SK1\\SSK1, K2\\SK1\\SSK2]");
+    }
+
+    @Test
+    public void whenFillGapsAndDesc() {
+        list = Departure.fillGaps(list);
+        list = Departure.desc(list);
+        Assert.assertEquals(list.toString(), "[K2, K2\\SK1, K2\\SK1\\SSK2, K2\\SK1\\SSK1, K1, K1\\SK2, K1\\SK1, K1\\SK1\\SSK2, K1\\SK1\\SSK1]");
+    }
+}
