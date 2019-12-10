f011ac11701c966563fb3f9984561d7f62201c68
vadimstr102 <vadimstr102@gmail.com>
2019-12-10 16:24:01 +0700

    Группировка по интересам. [#106993] (fixed)

--- chapter_002/src/main/java/ru/job4j/lambda/Group.java
+++ chapter_002/src/main/java/ru/job4j/lambda/Group.java
@@ -1,6 +1,5 @@
 package ru.job4j.lambda;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -9,12 +8,9 @@ import java.util.stream.Collectors;
 public class Group {
 
     public static Map<String, Set<String>> sections(List<Student> students) {
-        List<Pair> pairs = new ArrayList<>();
-        for (Student student : students) {
-            for (String unit : student.getUnits()) {
-                pairs.add(new Pair(student.getName(), unit));
-            }
-        }
+        List<Pair> pairs = students.stream().
+                flatMap(student -> student.getUnits().stream().map(u -> new Pair(student.getName(), u))).
+                collect(Collectors.toList());
         Map<String, Set<String>> result = pairs.stream().collect(
                 Collectors.groupingBy(Pair::getUnit, Collectors.mapping(Pair::getName, Collectors.toSet())));
         return result;
