841e8a85257189fc691aa0cc3f411fbd558acc39
vadimstr102 <vadimstr102@gmail.com>
2019-12-17 15:09:50 +0700

    5.1.4. Создать convert(Iterator<Iterator>)[#210915]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/Converter.java
@@ -0,0 +1,28 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Converter {
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            private Iterator<Integer> currentIt = it.next();
+
+            @Override
+            public boolean hasNext() {
+                if (!currentIt.hasNext() && it.hasNext()) {
+                    currentIt = it.next();
+                }
+                return currentIt.hasNext();
+            }
+
+            @Override
+            public Integer next() throws NoSuchElementException {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return currentIt.next();
+            }
+        };
+    }
+}
