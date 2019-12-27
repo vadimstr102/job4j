7be4bc01f47d082ad122035425b9f9268e855b8f
vadimstr102 <vadimstr102@gmail.com>
2019-12-27 11:55:11 +0700

    5.3.3.1 Очередь на двух стеках[#210910] (fixed)

--- chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
+++ chapter_005/src/main/java/ru/job4j/list/SimpleQueue.java
@@ -7,21 +7,18 @@ public class SimpleQueue<T> {
     private int count2 = 0;
 
     public T poll() {
-        while (count1 > 0) {
-            this.simpleStack2.push(this.simpleStack1.poll());
-            this.count2++;
-            this.count1--;
+        if (count2 == 0) {
+            while (count1 > 0) {
+                this.simpleStack2.push(this.simpleStack1.poll());
+                this.count2++;
+                this.count1--;
+            }
         }
         this.count2--;
         return this.simpleStack2.poll();
     }
 
     public void push(T value) {
-        while (count2 > 0) {
-            this.simpleStack1.push(this.simpleStack2.poll());
-            this.count1++;
-            this.count2--;
-        }
         this.count1++;
         this.simpleStack1.push(value);
     }
