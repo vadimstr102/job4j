078bb43cdb7fc71d4934b2f2c636a80959cb579c
vadimstr102 <vadimstr102@gmail.com>
2020-01-14 18:21:07 +0700

    4. Переопределить только equals[#210929]

--- chapter_005/src/main/java/ru/job4j/map/MapTest.java
+++ chapter_005/src/main/java/ru/job4j/map/MapTest.java
@@ -21,16 +21,24 @@ public class MapTest {
         }
 
         @Override
-        public int hashCode() {
-            return Objects.hash(name, children, birthday);
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return children == user.children
+                    && Objects.equals(name, user.name)
+                    && Objects.equals(birthday, user.birthday);
         }
     }
 
     public static void main(String[] args) {
         User user1 = new User("Ivan", 1, new GregorianCalendar(1980, Calendar.JANUARY, 1));
         User user2 = new User("Ivan", 1, new GregorianCalendar(1980, Calendar.JANUARY, 1));
-        System.out.println(user1.hashCode());
-        System.out.println(user2.hashCode());
+        System.out.println(user1.equals(user2));
         Map<User, Object> map = new HashMap<>();
         map.put(user1, "first");
         map.put(user2, "second");
