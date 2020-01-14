a13dd9dc0e06888bd885cfa42042330bb3ae2970
vadimstr102 <vadimstr102@gmail.com>
2020-01-14 17:59:25 +0700

    2. Не перекрывать equals hashCode[#210930]

--- chapter_005/src/main/java/ru/job4j/map/MapTest.java
+++ chapter_005/src/main/java/ru/job4j/map/MapTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapTest {
 
@@ -14,5 +17,19 @@ public class MapTest {
             this.children = children;
             this.birthday = birthday;
         }
+
+        @Override
+        public String toString() {
+            return "{" + this.name + "}";
+        }
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Ivan", 1, new GregorianCalendar(1980, Calendar.JANUARY, 1));
+        User user2 = new User("Ivan", 1, new GregorianCalendar(1980, Calendar.JANUARY, 1));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, "first");
+        map.put(user2, "second");
+        System.out.println(map);
     }
 }
