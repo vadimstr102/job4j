de2de9d0f4d31b291a22d4ec41d759beeeb13434
vadimstr102 <vadimstr102@gmail.com>
2020-02-16 17:19:21 +0700

    8. Реализовать собственную структуру данных - HashMap[#210933]

---
+++ chapter_005/src/main/java/ru/job4j/map/SimpleMap.java
@@ -0,0 +1,182 @@
+package ru.job4j.map;
+
+import java.util.*;
+
+public class SimpleMap<K, V> implements Iterable {
+    private Node<K, V>[] table;
+    private int size;
+    private int modCount;
+
+    public SimpleMap() {
+        this.table = new Node[16];
+    }
+
+    private class Node<K, V> {
+        private K key;
+        private V value;
+        private List<Node<K, V>> nodes;
+
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.nodes = new LinkedList<>();
+        }
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    private int hash(K key) {
+        int h = key.hashCode();
+        h = h ^ (h >>> 16);
+        return h & (this.table.length - 1);
+    }
+
+    public boolean insert(K key, V value) {
+        if (this.size + 1 >= this.table.length * 0.75) {
+            arrayDoubling();
+        }
+        Node<K, V> newNode = new Node<>(key, value);
+        int index = this.hash(key);
+        if (this.table[index] == null) {
+            return this.simpleAdd(newNode, index);
+        }
+        List<Node<K, V>> nodeList = this.table[index].nodes;
+        for (Node<K, V> node : nodeList) {
+            if (keyExistButValueNew(node, newNode) || collisionProcessing(node, newNode, nodeList)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean simpleAdd(Node<K, V> node, int index) {
+        this.table[index] = new Node<>(null, null);
+        this.table[index].nodes.add(node);
+        this.size++;
+        this.modCount++;
+        return true;
+    }
+
+    private boolean keyExistButValueNew(final Node<K, V> nodeFromList, final Node<K, V> newNode) {
+        if (nodeFromList.key.hashCode() == newNode.key.hashCode()
+                && Objects.equals(nodeFromList.key, newNode.key)
+                && !Objects.equals(nodeFromList.value, newNode.value)) {
+            nodeFromList.value = newNode.value;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean collisionProcessing(final Node<K, V> nodeFromList, final Node<K, V> newNode, final List<Node<K, V>> nodeList) {
+        if (nodeFromList.key.hashCode() == newNode.key.hashCode()
+                && !Objects.equals(nodeFromList.key, newNode.key)) {
+            nodeList.add(newNode);
+            this.size++;
+            this.modCount++;
+            return true;
+        }
+        return false;
+    }
+
+    private void arrayDoubling() {
+        Node<K, V>[] oldTable = this.table;
+        this.table = new Node[oldTable.length * 2];
+        this.size = 0;
+        for (Node<K, V> node : oldTable) {
+            if (node != null) {
+                for (Node<K, V> n : node.nodes) {
+                    insert(n.key, n.value);
+                }
+            }
+        }
+    }
+
+    public boolean delete(final K key) {
+        int index = this.hash(key);
+        if (this.table[index] != null) {
+            for (Node<K, V> node : this.table[index].nodes) {
+                if (key.hashCode() == node.key.hashCode()
+                        && Objects.equals(key, node.key)) {
+                    this.table[index].nodes.remove(node);
+                    this.size--;
+                    this.modCount++;
+                    break;
+                } else {
+                    return false;
+                }
+            }
+            if (this.table[index].nodes.size() == 0) {
+                this.table[index] = null;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public V get(final K key) {
+        V result = null;
+        int index = this.hash(key);
+        if (this.table[index] != null) {
+            for (Node<K, V> node : this.table[index].nodes) {
+                if (key.hashCode() == node.key.hashCode()
+                        && Objects.equals(key, node.key)) {
+                    result = node.value;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<V> iterator() {
+        return new Iterator<>() {
+            int arrayCounter = 0;
+            int valuesCounter = 0;
+            int itModCount = modCount;
+            Iterator<Node<K, V>> subIterator = null;
+
+            @Override
+            public boolean hasNext() {
+                if (this.itModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (this.valuesCounter == size) {
+                    return false;
+                }
+                if (subIterator == null || !subIterator.hasNext()) {
+                    if (moveToNextCell()) {
+                        subIterator = table[arrayCounter].nodes.iterator();
+                    } else {
+                        return false;
+                    }
+                }
+                return subIterator.hasNext();
+            }
+
+            private boolean moveToNextCell() {
+                if (arrayCounter != 0) {
+                    arrayCounter++;
+                }
+                while (arrayCounter < table.length && table[arrayCounter] == null) {
+                    arrayCounter++;
+                }
+                return arrayCounter < table.length && table[arrayCounter] != null;
+            }
+
+            @Override
+            public V next() {
+                if (this.itModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                valuesCounter++;
+                return subIterator.next().value;
+            }
+        };
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -0,0 +1,89 @@
+package ru.job4j.map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+public class SimpleMapTest {
+    SimpleMap<Integer, String> simpleMap = new SimpleMap<>();
+
+    @Before
+    public void start() {
+        simpleMap.insert(1, "one");
+        simpleMap.insert(2, "two");
+        simpleMap.insert(3, "three");
+        simpleMap.insert(4, "four");
+        simpleMap.insert(5, "five");
+    }
+
+    @Test
+    public void whenInsertKey() {
+        assertThat(simpleMap.size(), is(5));
+        simpleMap.insert(6, "six");
+        assertThat(simpleMap.size(), is(6));
+        assertThat(simpleMap.get(6), is("six"));
+    }
+
+    @Test
+    public void whenInsertExistsKey() {
+        assertThat(simpleMap.get(5), is("five"));
+        simpleMap.insert(5, "six");
+        assertThat(simpleMap.size(), is(5));
+        assertThat(simpleMap.get(5), is("six"));
+    }
+
+    @Test
+    public void whenDeleteExistsPair() {
+        assertThat(simpleMap.size(), is(5));
+        assertThat(simpleMap.delete(3), is(true));
+        assertThat(simpleMap.size(), is(4));
+    }
+
+    @Test
+    public void whenDeleteNoExistsPair() {
+        assertThat(simpleMap.size(), is(5));
+        assertThat(simpleMap.delete(7), is(false));
+        assertThat(simpleMap.size(), is(5));
+    }
+
+    @Test
+    public void whenGetValue() {
+        assertThat(simpleMap.get(1), is("one"));
+        assertNull(simpleMap.get(6));
+        assertThat(simpleMap.get(3), is("three"));
+        assertNull(simpleMap.get(7));
+        assertThat(simpleMap.get(5), is("five"));
+        assertNull(simpleMap.get(8));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenUseIterator() {
+        Iterator<String> it = simpleMap.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("one"));
+        assertThat(it.next(), is("two"));
+        assertThat(it.next(), is("three"));
+        assertThat(it.next(), is("four"));
+        assertThat(it.next(), is("five"));
+        assertThat(it.hasNext(), is(false));
+        it.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenUseIteratorAndModList() {
+        Iterator<String> it = simpleMap.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is("one"));
+        assertThat(it.next(), is("two"));
+        assertThat(it.hasNext(), is(true));
+        simpleMap.insert(6, "six");
+        it.next();
+    }
+}
