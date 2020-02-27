9f091ae00ae4a54bdce1b627bd58d6ad9ecab0cc
vadimstr102 <vadimstr102@gmail.com>
2020-02-27 20:40:49 +0700

    2. Тестирование IO[#210943]

--- chapter_006/src/test/java/AnalyzeTest.java
+++ chapter_006/src/test/java/AnalyzeTest.java
@@ -1,25 +1,37 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AnalyzeTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void whenUseAnalyze() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (BufferedReader reader = new BufferedReader(new FileReader("./src/test/resources/server.log"));
+             PrintWriter writer = new PrintWriter(source)) {
+            reader.lines()
+                    .filter(line -> !line.equals(""))
+                    .forEach(writer::println);
+        }
         Analyze analyze = new Analyze();
-        analyze.unavailable("./src/test/resources/server.log", "./src/test/resources/unavailable.csv");
-        BufferedReader read = new BufferedReader(new FileReader("./src/test/resources/unavailable.csv"));
-        assertThat(
-                read.readLine(),
-                is("10:57:01;10:59:01")
-        );
-        assertThat(
-                read.readLine(),
-                is("11:01:02;11:02:02")
-        );
+        analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader read = new BufferedReader(new FileReader(target))) {
+            assertThat(
+                    read.readLine(),
+                    is("10:57:01;10:59:01")
+            );
+            assertThat(
+                    read.readLine(),
+                    is("11:01:02;11:02:02")
+            );
+        }
     }
 }
