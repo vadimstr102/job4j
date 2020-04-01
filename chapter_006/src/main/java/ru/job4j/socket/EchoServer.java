d5b5a63d6e33843e0e10ae15a31973540c0c3c4f
vadimstr102 <vadimstr102@gmail.com>
2020-04-01 10:37:30 +0700

    0. Видео[#210954]

---
+++ chapter_006/src/main/java/ru/job4j/socket/EchoServer.java
@@ -0,0 +1,30 @@
+package ru.job4j.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            while (true) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    String str = in.readLine();
+                    if (str != null && str.contains("msg=Bye")) {
+                        break;
+                    }
+                    while (str != null && !str.isEmpty()) {
+                        System.out.println(str);
+                        str = in.readLine();
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                }
+            }
+        }
+    }
+}
