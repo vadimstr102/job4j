59691c0eb0f50b0beacb92bdd6f2233036ed1407
vadimstr102 <vadimstr102@gmail.com>
2020-04-03 10:13:10 +0700

    5. Создать программу 'Консольный чат'. [#210947] (fixed)

--- chapter_006/src/main/java/ru/job4j/io/ConsoleChat.java
+++ chapter_006/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,40 +9,49 @@ public class ConsoleChat {
     public void start() {
         String question;
         String answer;
-        //открываем потоки ввода из консоли, чтения ответов из файла, записи в лог-файл
-        try (
-                BufferedReader readQuestion = new BufferedReader(new InputStreamReader(System.in));
-                BufferedReader readAnswer = new BufferedReader(new FileReader("chapter_006/src/main/resources/answers.txt"));
-                BufferedWriter logWriter = new BufferedWriter(new FileWriter("chapter_006/src/main/resources/chat_log.txt"))
-        ) {
-            //заносим ответы из файла в List
-            List<String> answersPool = readAnswer.lines().collect(Collectors.toList());
+        List<String> answersPool = new ArrayList<>();
+        List<String> logList = new ArrayList<>();
+        //считываем ответы из файла в список
+        try (BufferedReader readAnswer = new BufferedReader(new FileReader("chapter_006/src/main/resources/answers.txt"))) {
+            answersPool = readAnswer.lines().collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //считываем строки из консоли
+        try (BufferedReader readQuestion = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println("Chat started");
             //читаем ввод из консоли в цикле
             question = readQuestion.readLine();
             while (!"закончить".equals(question)) {
-                //запись question в лог
-                logWriter.write(question + System.lineSeparator());
+                //запись question в лог-список
+                logList.add(question);
                 if ("стоп".equals(question)) {
                     question = readQuestion.readLine();
                     while (!"продолжить".equals(question)) {
-                        //запись question в лог
-                        logWriter.write(question + System.lineSeparator());
+                        //запись question в лог-список
+                        logList.add(question);
                         question = readQuestion.readLine();
                     }
-                    //запись question "продолжить" в лог
-                    logWriter.write(question + System.lineSeparator());
+                    //запись question "продолжить" в лог-список
+                    logList.add(question);
                 }
-                //присваиваем переменной answer случайный ответ из List
+                //присваиваем переменной answer случайный ответ из List, записываем её в лог-список и выводим на консоль
                 answer = answersPool.get((int) (Math.random() * answersPool.size()));
-                //запись answer в лог и вывод на консоль
-                logWriter.write(answer + System.lineSeparator());
+                logList.add(answer);
                 System.out.println(answer);
                 question = readQuestion.readLine();
             }
-            //запись question "закончить" в лог
-            logWriter.write(question + System.lineSeparator());
+            //запись question "закончить" в лог-список
+            logList.add(question);
             System.out.println("Chat is over");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //записываем лог-список в файл
+        try (BufferedWriter logWriter = new BufferedWriter(new FileWriter("chapter_006/src/main/resources/chat_log.txt"))) {
+            for (String s : logList) {
+                logWriter.write(s + System.lineSeparator());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
