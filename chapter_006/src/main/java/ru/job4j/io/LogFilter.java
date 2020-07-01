7c4793e570b6686902514557cbb8ecf1342bb60d
vadimstr102 <vadimstr102@gmail.com>
2020-07-01 12:05:21 +0700

    0.4. BufferedOutputStream [#252490]

--- chapter_006/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_006/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,8 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,10 +22,18 @@ public class LogFilter {
         return result;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(file)))) {
+            for (String s : log) {
+                out.println(s);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        for (String line : log) {
-            System.out.println(line);
-        }
+        save(log, "404.txt");
     }
 }
--- chapter_006/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_006/src/main/java/ru/job4j/io/ResultFile.java
@@ -1,11 +1,16 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("Hello, world!".getBytes());
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("result.txt")
+                ))) {
+            out.write("Hello, world!");
         } catch (Exception e) {
             e.printStackTrace();
         }
