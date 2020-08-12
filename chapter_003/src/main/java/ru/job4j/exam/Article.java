725cddad3ce8b59e71b37ee080d1b50ea924dd44
vadimstr102 <vadimstr102@gmail.com>
2020-08-12 16:52:24 +0700

    1. Генератор текста - Задача после экзамена по блоку Collections.Lite.

---
+++ chapter_003/src/main/java/ru/job4j/exam/Article.java
@@ -0,0 +1,13 @@
+package ru.job4j.exam;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+
+        for (String s : line.split(" ")) {
+            if (!origin.contains(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
