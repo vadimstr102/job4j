67e6b1659c2de1ff6abe301a3667e9c984d5c0b7
vadimstr102 <vadimstr102@gmail.com>
2020-08-17 21:55:36 +0700

    2. Проверить две строчки на идентичность - Задача после экзамена по блоку Collections.Lite. (fixed)

--- chapter_003/src/main/java/ru/job4j/exam/FreezeStr.java
+++ chapter_003/src/main/java/ru/job4j/exam/FreezeStr.java
@@ -1,13 +1,46 @@
 package ru.job4j.exam;
 
+import java.util.HashMap;
+
+/**
+ * @author Vadim Timofeev
+ */
 public class FreezeStr {
+    /**
+     * The method checks that the second line was obtained
+     * by rearranging the characters of the first line
+     *
+     * @param left
+     * @param right
+     * @return
+     */
     public static boolean eq(String left, String right) {
         if (left.length() != right.length()) {
             return false;
         }
-        for (String s : left.split("")) {
-            right = right.replaceFirst(s, "");
+        HashMap<String, Integer> leftMap = new HashMap<>();
+        HashMap<String, Integer> rightMap = new HashMap<>();
+        fillMap(leftMap, left.toLowerCase());
+        fillMap(rightMap, right.toLowerCase());
+        return leftMap.equals(rightMap);
+    }
+
+    /**
+     * Method fills the map.
+     * key - string (@param line) character
+     * value - the number of occurrences of a character in a string
+     *
+     * @param map
+     * @param line
+     */
+    private static void fillMap(HashMap<String, Integer> map, String line) {
+        for (String s : line.split("")) {
+            if (map.containsKey(s)) {
+                int count = map.get(s);
+                map.put(s, ++count);
+            } else {
+                map.put(s, 1);
+            }
         }
-        return right.length() == 0;
     }
 }
